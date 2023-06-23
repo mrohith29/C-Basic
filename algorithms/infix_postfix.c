63c6e31cc5f3a99bc4d3eac4ff3f349f27ff8d00
Noob Science <noobscience123@gmail.com>
2023-06-23 19:10:32 +0530

    Add Linked Lists

--- algorithms/infix_postfix.c
+++ algorithms/infix_postfix.c
@@ -1,38 +1,65 @@
-#include <ctype.h>
 #include <stdio.h>
-#define STACK_MAX 100
+#include <stdlib.h>
+#include <ctype.h>
+#define MAX_SIZE 20
 
-char stack[STACK_MAX];
 int top = -1;
-void push(char[], char);
-char pop(char[]);
-int getpriority(char);
+char stack[MAX_SIZE];
 
-int main() {
+void push(char stack[], char element){
+  if(top == MAX_SIZE-1){
+    printf("StackOverflow");
+  }
+  else{
+    top++;
+    stack[top] = element;
+  }
+}
+
+char pop(char stack[]){
+  if(top == -1){
+    printf("Stackunderflow");
+  }
+  else{
+    return stack[--top];
+  }
+}
+
+int getpriority(char op) {
+  if (op == '^')
+    return 2;
+  else if (op == '/' || op == '*' || op == '%')
+    return 1;
+  else if (op == '+' || op == '-')
+    return 0;
+  else
+    return -1;
+}
+
+int main(){
   char infix[100], postfix[100], temp;
-  int i, j = 0;
+  int i,j=0;
   scanf("%s", infix);
-  for (i = 0; infix[i] != '\0'; i++) {
-    if (infix[i] == '(')
+  for(i = 0; i!='\0'; i++){
+    if(infix[i] == '('){
       push(stack, infix[i]);
-    else if (isalpha(infix[i]) || isdigit(infix[i])) {
-      postfix[j] = infix[i];
+    }
+    else if(isalpha(infix[i]) || isdigit(infix[i])){
+      postfix[i] = infix[i];
       j++;
-    } 
-    else if (infix[i] == '+' || infix[i] == '-' || infix[i] == '*' ||
-               infix[i] == '/' || infix[i] == '%' || infix[i] == '^') {
-      while ((getpriority(stack[top])) >= getpriority(infix[i])) {
-        postfix[j] = pop(stack);
+    }
+    else if(infix[i] == '+' || infix[i] == '-' || infix[i] == '*' || infix[i] == '/' || infix[i] == '%' || infix[i] == '^'){
+      while(getpriority(stack[top]) >= getpriority(infix[i])){
+        postfix[i]  = pop(stack);
         j++;
       }
       push(stack, infix[i]);
-    } 
-    else if (infix[i] == ')') {
-      while (stack[top] != '(') {
-        postfix[j] = pop(stack);
+    }
+    else if(infix[i] == ')'){
+      while(stack[top] != '('){
+        postfix[i] = pop(stack);
         j++;
       }
-      temp = pop(stack);
     }
   }
   while (top != -1) {
@@ -43,34 +70,4 @@ int main() {
   printf("%s", postfix);
 
   return 0;
-}
-void push(char stack[], char ele) {
-  if (top == STACK_MAX - 10) {
-    printf("stack overflow");
-  } 
-  else {
-    top++;
-    stack[top] = ele;
-  }
-}
-char pop(char stack[]) {
-  char val;
-  if (top == -1) {
-    printf("underflow");
-  } 
-  else {
-    val = stack[top];
-    top--;
-  }
-  return val;
-}
-int getpriority(char op) {
-  if (op == '^')
-    return 2;
-  else if (op == '/' || op == '*' || op == '%')
-    return 1;
-  else if (op == '+' || op == '-')
-    return 0;
-  else
-    return -1;
 }
---
+++ linked/singly_linked_lists.c
@@ -0,0 +1,95 @@
+// Singly Linked Lists in c are a data structure that can be used to store a list of values.
+// They are defined as structures with a pointer to the next node in the list.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node *next;
+} *head = NULL;
+
+void display(){
+    struct node *temp = head;
+    while(temp != NULL){
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+void insert_at_beginning(int ele){
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = ele;
+    new_node->next = head;
+    head = new_node;
+}
+
+void insert_at_end(int ele){
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = ele;
+    new_node->next = NULL;
+    struct node *temp = head;
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = new_node;
+}
+
+void insert_at_position(int ele, int pos){
+    struct node *new_node;
+    new_node = (struct node *)malloc(sizeof(struct node));
+    new_node->data = ele;
+    struct node *temp = head;
+    for(int i = 0; i < pos-1; i++){
+        temp = temp->next;
+    }
+    new_node->next = temp->next;
+    temp->next = new_node;
+}
+
+void delete_at_beginning(){
+    struct node *temp = head;
+    head = head->next;
+    free(temp);
+}
+
+int main(){
+    int choice, ele, pos;
+    while(1){
+        printf("\n1. Display\n2. Insert at beginning\n3. Insert at end\n4. Insert at position\n5. Delete at beginning\n6. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice){
+            case 1:
+                display();
+                break;
+            case 2:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &ele);
+                insert_at_beginning(ele);
+                break;
+            case 3:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &ele);
+                insert_at_end(ele);
+                break;
+            case 4:
+                printf("Enter the element to be inserted: ");
+                scanf("%d", &ele);
+                printf("Enter the position: ");
+                scanf("%d", &pos);
+                insert_at_position(ele, pos);
+                break;
+            case 5:
+                delete_at_beginning();
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("Invalid choice\n");
+        }
+    }
+    return 0;
+}
